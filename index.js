728426e1f50e0e650cee8e3f5863995ad9d3366a
Joseph Dotson <dotsonjb14@gmail.com>
2018-08-31 21:18:08 -0400

    Initial version

---
+++ index.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+const request = require('request'),
+      commander = require('commander'),
+      fs = require('fs');
+
+const EOL = "\n";
+
+commander
+  .option('-u --uri <uri>', 'the uri of the swagger (required)')
+  .option('-o --out <outFile>', 'output file path', '.\\out.d.ts')
+  .option('-d --dry-run', 'print the output instead of writing it to disk')
+  .parse(process.argv);
+
+if(commander.uri === void 0) {
+  commander.outputHelp();
+  return;
+}
+
+var fileBuffer = "";
+
+request.get(commander.uri, (err, resp, body) => {
+  main(JSON.parse(body));
+});
+
+function main(data) {
+  for(let className in data.definitions) {
+    buildClass(className, data.definitions[className])
+  }
+
+  if(commander.dryRun) {
+    console.log(fileBuffer);
+  }
+  else {
+    fs.writeFileSync(commander.out, fileBuffer);
+  }
+}
+
+function buildClass(className, classData) {
+  fileBuffer += `export interface ${className} {${EOL}`;
+  let props = classData.properties;
+
+  for(let propName in props) {
+    fileBuffer += `  ${propName}: ${buildTypeDef(props[propName])};${EOL}`;
+  }
+
+  fileBuffer += `}${EOL}${EOL}`;
+}
+
+function buildTypeDef(prop) {
+  if(prop.$ref !== void 0) {
+    return getObjectName(prop.$ref);
+  }
+
+  let mappedType = mapType(prop.type);
+
+  if(mappedType !== null) {
+    return mappedType;
+  }
+
+  if(prop.type === 'array') {
+    let itemsType = mapType(prop.items.type);
+    if(itemsType !== null) {
+      return `${itemsType}[]`;
+    }
+
+    if(prop.items.$ref !== void 0) {
+      // it's an object
+      return `${getObjectName(prop.items.$ref)}[]`;
+    }
+  }
+
+  return 'notMapped'
+}
+
+function getObjectName(ref) {
+  let parts = ref.split('/');
+  return  parts[parts.length -1];
+}
+
+function mapType(type) {
+  switch(type) {
+    case 'string':
+      return 'string'
+    case 'integer':
+      return 'number'
+    case 'boolean':
+      return 'boolean'
+    default:
+      return null;
+  }
+}
